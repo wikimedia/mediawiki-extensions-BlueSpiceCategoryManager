eb8dcce77392fa47f575a3dec8a6054f415b5fb2
Leonid Verhovskij <l.verhovskij@gmail.com>
2017-10-10 13:43:56 +0200

    remove deprecated wikiadmin usage, todo: implement new alternative for wikiadmin listing
    Change-Id: I1656dbc603bb9cdc305564d89a5ab0631f2324a1

--- includes/BlueSpiceCategoryManager.php
+++ includes/BlueSpiceCategoryManager.php
@@ -1,18 +1,5 @@
 <?php
 class BlueSpiceCategoryManager extends BsExtensionMW {
-
-	public function __construct() {
-		wfProfileIn( 'BS::'.__METHOD__ );
-		WikiAdmin::registerModuleClass( 'BlueSpiceCategoryManager', array(
-			'image' => '/extensions/BlueSpiceCategoryManager/resources/images/bs-btn_categorymanager.png',
-			'level' => 'wikiadmin',
-			'message' => 'bs-categorymanager-label',
-			'iconCls' => 'bs-icon-tag',
-			'permissions' => [ 'categorymanager-viewspecialpage' ],
-		) );
-		wfProfileOut( 'BS::'.__METHOD__ );
-	}
-
 	protected function initExt() {
 		BsCore::getInstance()->registerPermission( 'categorymanager-viewspecialpage', array( 'sysop' ), array( 'type' => 'global' ) );
 	}
