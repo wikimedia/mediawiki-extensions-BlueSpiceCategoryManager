8cfd1d30bf84f6477ad104edbd0dc8013e7eefde
d.savuljesku <savuljesku@hallowelt.com>
2021-09-17 09:09:42 +0200

    Implement new Statistics snapshot providers
    Requires I999788b1dfef1ac20cf6df6a265b2983ab571e50
    
    [3.2.x]
    
    ERM 25753
    
    Change-Id: I1504a34b0a8a0ef78aece3dbd5ca2b07f33d5047

---
+++ src/Statistics/Report/CategoryPages.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace BlueSpice\CategoryManager\Statistics\Report;
+
+use BlueSpice\ExtendedStatistics\ClientReportHandler;
+use BlueSpice\ExtendedStatistics\IReport;
+use MWNamespace;
+
+class CategoryPages implements IReport {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getSnapshotKey() {
+		return 'cm-categories';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getClientData( $snapshots, array $filterData, $limit = 20 ): array {
+		$namespace = $this->getNamespace( $filterData );
+		$categories = $filterData['categories'] ?? [];
+		if ( empty( $categories ) ) {
+			return [];
+		}
+		$categories = array_map( static function ( $cat ) {
+			return str_replace( ' ', '_', $cat );
+		}, $categories );
+
+		$processed = [];
+		foreach ( $snapshots as $snapshot ) {
+			if ( $namespace ) {
+				$data = $snapshot->getData()['namespaces'];
+				if ( !isset( $data[$namespace] ) ) {
+					return [];
+				}
+				$data = $data[$namespace];
+			} else {
+				$data = $snapshot->getData()['all'];
+			}
+			foreach ( $data as $cat => $props ) {
+				if ( !in_array( $cat, $categories ) ) {
+					continue;
+				}
+				$processed[] = [
+					'name' => $snapshot->getDate()->forGraph(),
+					'line' => $cat,
+					'value' => $props['pages']
+				];
+			}
+		}
+
+		return $processed;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getClientReportHandler(): ClientReportHandler {
+		return new ClientReportHandler(
+			[ 'ext.bluespice.categorymanager.statistics' ],
+			'bs.categoryManager.report.CategoryPagesReport'
+		);
+	}
+
+	/**
+	 * @param array $filterData
+	 * @return string|null
+	 */
+	private function getNamespace( array $filterData ) {
+		$namespace = $filterData['namespace'] ?? null;
+		if ( $namespace === null ) {
+			return null;
+		}
+		if ( $namespace === '' ) {
+			return null;
+		}
+
+		if ( (int)$namespace === 0 ) {
+			return '-';
+		}
+
+		return MWNamespace::getCanonicalName( $namespace );
+	}
+}
