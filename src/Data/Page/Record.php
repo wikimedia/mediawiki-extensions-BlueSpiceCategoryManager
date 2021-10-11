881e84896319c36fcf5c81417faf0e88bd9dec7a
Alexander Vorwerk <alexander.vorwerk@stud.uni-goettingen.de>
2021-10-11 13:48:22 +0200

    Add visibility to constants
    We support PHP 7.3.19 or later, so we should declare the visibility of
    constants.
    
    Bug: T237840
    Change-Id: I909004cc3a02e82f6c494fb9eba18c6f14faf866

--- src/Data/Page/Record.php
+++ src/Data/Page/Record.php
@@ -3,6 +3,6 @@
 namespace BlueSpice\CategoryManager\Data\Page;
 
 class Record extends \BlueSpice\Data\Page\Record {
-	const CATEGORIES = 'categories';
-	const PREFIXED_TEXT = 'page_prefixedtext';
+	public const CATEGORIES = 'categories';
+	public const PREFIXED_TEXT = 'page_prefixedtext';
 }
