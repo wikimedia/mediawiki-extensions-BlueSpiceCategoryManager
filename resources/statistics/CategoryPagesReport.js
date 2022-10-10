d6e39b9f80c67cdc3ae7463bfb8a9e9007a00f20
miriam schlindwein <schlindwein@hallowelt.com>
2022-10-10 14:00:00 +0200

    Improve UI statistics
    Fix namespace selector and add axis label
    
    ERM29537
    
    [4.2.x]
    
    Change-Id: Id2844206b95b509c8ee458578724db9e9818712a

--- resources/statistics/CategoryPagesReport.js
+++ resources/statistics/CategoryPagesReport.js
@@ -13,11 +13,18 @@
 		return [
 			new bs.aggregatedStatistics.filter.IntervalFilter(),
 			new bs.aggregatedStatistics.filter.CategoryMultiFilter( { required: true } ),
-			new bs.aggregatedStatistics.filter.NamespaceFilter( { onlyContentNamespaces: true } ),
+			new bs.aggregatedStatistics.filter.NamespaceFilter( { onlyContentNamespaces: false } ),
 		];
 	};
 
 	bs.categoryManager.report.CategoryPagesReport.prototype.getChart = function () {
 		return new bs.aggregatedStatistics.charts.LineChart();
 	};
+
+	bs.categoryManager.report.CategoryPagesReport.prototype.getAxisLabels = function () {
+		return {
+			value: mw.message( "bs-categorymanager-statistics-report-category-pages-axis-value" ).text()
+		};
+	};
+
 } )( mediaWiki, jQuery , blueSpice);
