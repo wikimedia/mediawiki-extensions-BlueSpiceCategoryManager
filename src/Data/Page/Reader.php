5f1563b8fdd26ab28eaf83e17774ab54a33d8bb8
Hamish <hamish.slater@hallowelt.com>
2026-07-13 15:40:09 +0700

    phpcs

--- src/Data/Page/Reader.php
+++ src/Data/Page/Reader.php
@@ -9,16 +9,14 @@ use Wikimedia\Rdbms\LoadBalancer;
 
 class Reader extends PageReader {
 	/**
-	 *
 	 * @param LoadBalancer $loadBalancer
 	 * @param IContextSource|null $context
 	 */
-	public function __construct( $loadBalancer, IContextSource $context = null ) {
+	public function __construct( $loadBalancer, ?IContextSource $context = null ) {
 		parent::__construct( $loadBalancer, $context, $context->getConfig() );
 	}
 
 	/**
-	 *
 	 * @param ReaderParams $params
 	 * @return PrimaryDataProvider
 	 */
@@ -27,7 +25,6 @@ class Reader extends PageReader {
 	}
 
 	/**
-	 *
 	 * @return null
 	 */
 	protected function makeSecondaryDataProvider() {
@@ -35,7 +32,6 @@ class Reader extends PageReader {
 	}
 
 	/**
-	 *
 	 * @return Schema
 	 */
 	public function getSchema() {
--- src/Hook/BSPageStoreDataProviderBeforeAppendRow.php
+++ src/Hook/BSPageStoreDataProviderBeforeAppendRow.php
@@ -1,29 +1,5 @@
 <?php
-/**
- * Hook handler base class for BlueSpice hook BSPageStoreDataProviderBeforeAppendRow
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * This file is part of BlueSpice MediaWiki
- * For further information visit https://bluespice.com
- *
- * @author     Patric Wirth
- * @package    BlueSpiceCategoryManager
- * @copyright  Copyright (C) 2020 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
- * @filesource
- */
+
 namespace BlueSpice\CategoryManager\Hook;
 
 use BlueSpice\CategoryManager\Data\Page\PrimaryDataProvider;
@@ -36,25 +12,21 @@ use MediaWiki\Title\Title;
 abstract class BSPageStoreDataProviderBeforeAppendRow extends Hook {
 
 	/**
-	 *
 	 * @var PrimaryDataProvider
 	 */
 	protected $dataProvider = null;
 
 	/**
-	 *
 	 * @var Record
 	 */
 	protected $record = null;
 
 	/**
-	 *
 	 * @var Title
 	 */
 	protected $title = null;
 
 	/**
-	 *
 	 * @param PrimaryDataProvider $dataProvider
 	 * @param Record $record
 	 * @param Title $title
@@ -73,7 +45,6 @@ abstract class BSPageStoreDataProviderBeforeAppendRow extends Hook {
 	}
 
 	/**
-	 *
 	 * @param IContextSource $context
 	 * @param Config $config
 	 * @param PrimaryDataProvider $dataProvider
